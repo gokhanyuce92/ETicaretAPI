42d29628d8d41b87ca0386fc205ff694c5d13441
gokhanyuce <g.yuce92@gmail.com>
2023-10-14 22:35:21 +0300

    RolePermissionFilter Oluşturma ve Yetki Kontrolü

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,5 +1,6 @@
 using ETicaretAPI.API.Configurations.ColumnWriters;
 using ETicaretAPI.API.Extensions;
+using ETicaretAPI.API.Filters;
 using ETicaretAPI.Application;
 using ETicaretAPI.Application.Abstractions.Storage;
 using ETicaretAPI.Application.Validators.Products;
@@ -60,7 +61,11 @@ Logger log = new LoggerConfiguration()
 
 builder.Host.UseSerilog(log);
 
-builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>())
+builder.Services.AddControllers(options=> 
+{
+    options.Filters.Add<ValidationFilter>();
+    options.Filters.Add<RolePermissionFilter>();
+})
     .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
     .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
     
